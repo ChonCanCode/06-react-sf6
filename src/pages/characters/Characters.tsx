c3afcc8582d341e0339df610ba8bfebf9b2f6a3f
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-07-14 09:23:15 +0100

    Day 2

---
+++ src/pages/characters/Characters.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Characters() {
+  return (
+    <>
+      <h1>I am at Character Page</h1>
+      <Link to="/Bison">Bison</Link>
+    </>
+  );
+}
