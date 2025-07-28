48151e073a4c7380f51e45f4cce1c56933184c51
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-07-28 16:44:09 +0100

    started writng function for flowchart search

--- src/pages/Flowchart.tsx
+++ src/pages/Flowchart.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+type comboType = {
+  tags: string;
+  meter: number;
+  damage: number;
+  moves: string[];
+};
+
+type dataType = {
+  data: comboType[];
+};
 
 export default function Flowchart() {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [searchResult, setSearchResult] = useState();
+
+  function handleSearch() {}
+
   return (
     <>
-      <h1>Flowchart</h1>
-      <Link to="/Bison">Bison</Link>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search by tags..."
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
     </>
   );
 }
