625d7ab19239f6db0cd53c013443bed6977b2dbd
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-08-17 17:16:31 +0100

    deploy on github

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/06-react-sf6/",
   plugins: [tailwindcss(), react()],
 });
